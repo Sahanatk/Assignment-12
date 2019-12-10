c5f13c914b30d5de1f45fbc501e8dfcbcf62bc12
Sahana Tunga <sahanatk23@gmail.com>
2019-12-10 15:17:34 -0800

    Assignment-12

---
+++ react-assignment/src/App.js
@@ -0,0 +1,15 @@
+/*jslint es6 */
+import React ,{Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Products from './Products';
+
+function App() {
+  return (
+    <div>
+      <Products></Products>
+   </div>
+  );
+}
+
+export default App;
